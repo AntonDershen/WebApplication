50666b03c62217e87f8f374f8652aa4a2753d296
AntonDershen <dershen95@gmail.com>
2015-06-21 02:41:54 +0300

    2

---
+++ Dal/Mapper/AuthorizationMapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dal.Interface.DTO;
+using ORM;
+namespace Dal.Mapper
+{
+    public static class AuthorizationMapper
+    {
+        public static DalAuthorization ToDalAuth(this Authorization auth)
+        {
+            return new DalAuthorization()
+            {
+                Id = auth.Id,
+                Login = auth.Login,
+                Password = auth.Password
+            };
+        }
+        public static Authorization ToAuth(this DalAuthorization dalAuth)
+        {
+            return new Authorization()
+            {
+                Id = dalAuth.Id,
+                Login = dalAuth.Login,
+                Password = dalAuth.Password
+            };
+        }
+
+    }
+}
---
+++ Dal/Mapper/UserMapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dal.Interface.DTO;
+using ORM;
+namespace Dal.Mapper
+{
+    public static class UserMapper
+    {
+        public static DalUser ToDalUser(this User user)
+        {
+            if (user != null)
+            {
+                return new DalUser()
+                {
+                    Id = user.Id,
+                    UserName = user.UserName
+                };
+            }
+            return null;
+        }
+        public static User ToUser(this DalUser dalUser)
+        {
+            if (dalUser != null)
+            {
+                return new User()
+                {
+                    Id = dalUser.Id,
+                    UserName = dalUser.UserName
+                };
+            }
+            return null;
+        }
+
+    }
+}
